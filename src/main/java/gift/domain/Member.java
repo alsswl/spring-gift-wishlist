d0ad1b951580d3a8ec0cd5d3743b6c01f368759a
alsswl <alswl4223@naver.com>
2024-07-05 02:12:55 +0900

    feat : member도메인에 생성자 구현

--- src/main/java/gift/domain/Member.java
+++ src/main/java/gift/domain/Member.java
@@ -3,4 +3,9 @@ package gift.domain;
 public class Member {
     String id;
     String passwd;
+
+    public Member(String id, String passwd) {
+        this.id = id;
+        this.passwd = passwd;
+    }
 }
